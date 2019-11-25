809f5e98f1718ab52b66598a4d80ab215b5e6799
Joshua Dagda <38403917+jedagda@users.noreply.github.com>
2019-11-25 15:27:01 -0700

    Add files via upload

--- contracts/Raptor.sol
+++ contracts/Raptor.sol
@@ -3,24 +3,24 @@ pragma solidity ^0.5.7;
 import "./SmellTracker.sol";
 
 contract Raptor{
-    
+
     address private manager;//Address of manager for high level operation clearance
     uint private totalReward = 0;
     bool private isActive = false;
     string json = "{ \"key1\": { \"key1.1\": \"value\", \"key1.2\": 3, \"key1.3\": true } }";
-    
+
     //Address of smell tracker for later use
     address private smellTrackerAddress;
-    
+
     constructor(address _smellTrackerAddress) public{
         //Save address of smell tracker to allow use
         smellTrackerAddress = _smellTrackerAddress;
     }
-    
+
     //=====
     //State modification
     //=====
-    
+
     /**
 		setup()
 		initial contract setup
@@ -28,18 +28,18 @@ contract Raptor{
 		@param rewardPerSmell - reward per smell
 		@return Success flag
     **/
-    function setup(string memory initialSmellReport, uint rewardPerSmell) public payable returns (bool){
+    function setup() public payable{
         require(!isActive);
-        
+
         //Save details
         manager = msg.sender;
         totalReward = msg.value;
-        
+
         //Initial smell check
-        uint smells = SmellTracker(smellTrackerAddress).checkSmells(initialSmellReport);
-        isActive = true;
+        uint numOfsmells = SmellTracker(smellTrackerAddress).checkNumSmells();
+        string querySmells = SmellTracker(smellTrackerAddress).checkSmells();
     }
-    
+
     /**
 		cancelProgram()
 		Refund reward remaining to manager and inactivate reward program
@@ -48,13 +48,13 @@ contract Raptor{
     function cancelProgram() public payable returns (bool){
 		require(msg.sender==manager, "Manager required for this operation");
 		require(isActive, "Reward program is inactive");
-		
+
 		msg.sender.transfer(totalReward);
 		isActive = false;
-		
+
 		return true;
 	}
-	
+
 	/**
 		status()
 		Returns active flag
@@ -63,7 +63,7 @@ contract Raptor{
 	function status() public returns (bool){
 		return isActive;
 	}
-    
+
     /**
 		checkSmells()
 		Used to update smell report and reward for smell removal
@@ -71,13 +71,13 @@ contract Raptor{
     **/
     function checkSmells(string memory smellReport, bool isInitial) public payable returns (uint){
         require(isActive);
-        
+
         uint smells = SmellTracker(smellTrackerAddress).checkSmells(smellReport);
     }
-    
+
     //To be called to reward for removing smells
     function reward(uint smellsRemoved) private returns (bool){
-        
+
     }
-    
+
 }
